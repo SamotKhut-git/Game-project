e7f33788b984e7160370b4e156d61d8b86a173d2
SamotKhut-git <khotsamot99@gmail.com>
2024-03-13 22:42:06 +0700

    final

--- Rock-Paper-Scissor-Game/index.js
+++ Rock-Paper-Scissor-Game/index.js
@@ -2,6 +2,10 @@ const choises = ["Rock", "Paper", "Scissor"];
 const playerChoiseDisplay = document.getElementById("playerChoisesDisplay");
 const computerChoiseDisplay = document.getElementById("computerChoisesDisplay");
 const resultDisplay = document.getElementById("resultDisplay");
+const displayPlayerScore = document.getElementById("displayPlayerScore");
+const displayComputeScore = document.getElementById("displayComputerScore");
+let playerScore = 0;
+let computerScore = 0;
 
 function playerChoises(game) {
 	const computerChoise = choises[Math.floor(Math.random() * 3)];
@@ -21,5 +25,8 @@ function playerChoises(game) {
 	}
 	playerChoiseDisplay.textContent = `Player:${game}`;
 	computerChoiseDisplay.textContent = `Computer:${computerChoise}`;
-	resultDisplay.textContent = `Result:${result}`;
+	resultDisplay.textContent = result;
+	result === "You Win" ? displayPlayerScore.textContent = `Player Score:${playerScore += 1}` : result === "You Lose" ? displayComputeScore.textContent = `Computer Score:${computerScore += 1}` : "";
+
+
 }
